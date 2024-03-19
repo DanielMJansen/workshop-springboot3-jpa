3ecec8a1b44a283fc0dfa72ebe2d556eb654c41a
Daniel Jansen <danielodie@hotmail.com>
2024-03-19 11:30:36 -0300

    H2 Database, test profile, JPA

--- src/main/java/com/danieljansen/course/entities/User.java
+++ src/main/java/com/danieljansen/course/entities/User.java
@@ -2,9 +2,19 @@ package com.danieljansen.course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -13,6 +23,7 @@ public class User implements Serializable {
 
 	public User() {
 	}
+
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
